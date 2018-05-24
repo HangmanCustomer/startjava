50f5dd318ce46d233e815a0c54a8a9c608bfe0f4
HangmanCustomer <anotherdawnrises@gmail.com>
2018-05-24 20:33:50 +0300

    Added repeat or end form

--- lesson2/CalculatorTest.java
+++ lesson2/CalculatorTest.java
@@ -4,12 +4,21 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calc = new Calculator();
-        System.out.println("Enter the first number");
-        calc.setA(scanner.nextInt());
-        System.out.println("Enter the operation");
-        calc.setCh(scanner.next().charAt(0));
-        System.out.println("Enter the second number");
-        calc.setB(scanner.nextInt());
-        calc.calcMath();
+        String yesOrNo = "Y";
+        while(yesOrNo.equals("Y")){
+            System.out.println("Begining calculate\n");
+            System.out.println("Enter the first number");
+            calc.setA(scanner.nextInt());
+            System.out.println("Enter the operation");
+            calc.setCh(scanner.next().charAt(0));
+            System.out.println("Enter the second number");
+            calc.setB(scanner.nextInt());
+            calc.calcMath();
+            do{
+            System.out.println("Repeat calculate press [Y/N]");
+            yesOrNo = scanner.next();
+             } while(!yesOrNo.equals("Y") && !yesOrNo.equals("N"));
+        }
+        System.out.println("Calculator is close");
     }
 }
