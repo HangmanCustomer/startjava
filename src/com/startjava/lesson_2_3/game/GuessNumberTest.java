9c049641da614292d355b789e7b224e13270de06
HangmanCustomer <anotherdawnrises@gmail.com>
2018-05-28 20:51:53 +0300

    Added game repeater

--- src/com/startjava/lesson_2_3/game/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3/game/GuessNumberTest.java
@@ -1,6 +1,8 @@
 package com.startjava.lesson_2_3.game;
 public class GuessNumberTest{
 	public static void main(String[] args) {
+		String yesOrNo = "y";
+		while(yesOrNo.equals("y")){
 		GuessNumber guessNumber = new GuessNumber();
 		System.out.println("++++ ---- Game Start ---- ++++\nNumbers from 0 to 10\n");
 		System.out.println("Set name of player I");
@@ -8,5 +10,11 @@ public class GuessNumberTest{
 		System.out.println("Set name of player II");
 		guessNumber.player02.setName(guessNumber.scanner.next());
 		guessNumber.guessingProcess();
+			do {
+				System.out.println("Do you wonna play again [Y/N]");
+				yesOrNo = guessNumber.scanner.next();
+				}
+				while (yesOrNo.equals("n") && yesOrNo.equals("y"));
+				}
+			}
 	}
-}
