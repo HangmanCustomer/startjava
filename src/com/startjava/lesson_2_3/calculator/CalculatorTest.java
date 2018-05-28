e41757edc0ecc8a1303697c09acac64628d104c7
HangmanCustomer <anotherdawnrises@gmail.com>
2018-05-28 21:15:46 +0300

    Added tiny changes

--- src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
@@ -5,20 +5,20 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calc = new Calculator();
-        String yesOrNo = "Y";
-        while(yesOrNo.equals("Y")){
+        String yesOrNo = "y";
+        while(yesOrNo.equals("y")){
             System.out.println("Begining calculate\n");
-            System.out.println("Enter the first number");
+            System.out.print("Enter the first number ");
             calc.setA(scanner.nextInt());
-            System.out.println("Enter the operation");
+            System.out.print("Enter the operation ");
             calc.setCh(scanner.next().charAt(0));
-            System.out.println("Enter the second number");
+            System.out.print("Enter the second number ");
             calc.setB(scanner.nextInt());
             calc.calcMath();
             do{
-            System.out.println("Repeat calculate press [Y/N]");
+            System.out.print("Repeat calculate press [Y/N] ");
             yesOrNo = scanner.next();
-             } while(!yesOrNo.equals("Y") && !yesOrNo.equals("N"));
+             } while(!yesOrNo.equals("y") && !yesOrNo.equals("n"));
         }
         System.out.println("Calculator is close");
     }
